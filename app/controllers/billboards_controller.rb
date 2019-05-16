fddd999e9aa2e3515e827bcf7363853406833595
Chris McGivney <cmmcgivney84@gmail.com>
2019-05-16 17:22:13 -0600

    moving right along

--- app/controllers/billboards_controller.rb
+++ app/controllers/billboards_controller.rb
@@ -1,5 +1,9 @@
 class BillboardsController < ApplicationController
+  before_action :set_bill, only: [:edit, :show, :update, :destroy]
+
+  
   def index
+    @billboards = Billboard.all
   end
 
   def show
@@ -9,5 +13,37 @@ class BillboardsController < ApplicationController
   end
 
   def new
+    @billboard = Billboard.new
   end
+  
+  def create  
+    @billboard = Billboard.new(bill_params)
+    if @billboard.save
+      redirect_to billboard_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @billboard.update(bill_params)
+      redirect_to billboard_path(@billboard)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @billboard.destroy
+    redirect_to billboard_path
+  end
+
+  private
+  def set_bill
+    @billboard = Billboard.find(params[:id])
+  end
+  def bill_params
+    params.require(:billboard).permit(:name)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,10 @@
 Rails.application.routes.draw do
-  get 'artists/index'
-  get 'artists/show'
-  get 'artists/edit'
-  get 'artists/new'
-  get 'songs/index'
-  get 'songs/show'
-  get 'songs/edit'
-  get 'songs/new'
-  get 'billboards/index'
-  get 'billboards/show'
-  get 'billboards/edit'
-  get 'billboards/new'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+root 'billboards#index'
+
+resources :billboards do 
+  resources :song
+end
+resources :artists do 
+  resources :songs
+end
 end
